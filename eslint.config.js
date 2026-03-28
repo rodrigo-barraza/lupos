f6773f6071c346a3c316c997c75c7e9666d9bc9d
Rodrigo Barraza <hello@rod.dev>
2026-03-27 21:23:14 -0700

    refactor: migrate eslint configuration to .js and update linting and formatting scripts in package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,11 @@
 import js from "@eslint/js";
-import globals from "globals";
 import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   prettierConfig,
   {
-    files: ["**/*.{js,mjs,cjs}"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
@@ -29,7 +28,11 @@ export default [
     },
   },
   {
-    files: ["wrappers/PuppeteerWrapper.js", "wrappers/PuppeteerWrapper2.js"],
+    files: [
+      "wrappers/PuppeteerWrapper.js",
+      "wrappers/PuppeteerWrapper2.js",
+      "services/ScraperService.js",
+    ],
     languageOptions: {
       globals: {
         document: "readonly",
